a027b68132d72008fcf84b0cd02f5b21b9cd6bb5
koowyzrk <banaszkiewiczjasiek@gmail.com>
2025-12-06 14:10:51 +0100

    adding mock for database connection

---
+++ modules/analysis-report/src/main/java/org/example/MockDataPersistenceService.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.Documents.DocumentScheme;
+import org.example.Documents.IDocument;
+
+public class MockDataPersistenceService implements IDataPersistenceService {
+  @Override
+  public IDocument saveDocument(DocumentScheme scheme) {
+    // return new Report();
+  }
+}
