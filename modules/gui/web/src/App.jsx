57f6a94165cc8c724a3b589f63c05c85c89b2fc0
Kacper Kinasiewicz <kacper.kinasiewicz.1@gmail.com>
2025-12-06 22:37:58 +0100

    Small GUI update

--- modules/gui/web/src/App.jsx
+++ modules/gui/web/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Login from './Panels/LoginPanel';
 import AdminPanel from './Panels/AdminPanel';
